1a750ae39b9c9fdbc8a3ddf109226bf6c94e0cff
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-13 20:15:51 +0200

    remove extra config object

--- src/sahara/search.cpp
+++ src/sahara/search.cpp
@@ -1,5 +1,4 @@
 #include "utils.h"
-#include "argp.h"
 
 #include <clice/clice.h>
 #include <cstdio>
@@ -51,32 +50,24 @@ auto cliNoReverse = clice::Argument{ .parent = &cli,
                                      .arg    = "--no-reverse",
                                      .desc   = "do not search for reversed complements",
 };
+
+enum class SearchMode { All, BestHits };
 auto cliSearchMode = clice::Argument{ .parent = &cli,
-                                     .arg    = "--search_mode",
-                                     .desc   = "do not search for reversed complements",
-                                     .value  = Config::Mode::All,
-                                     .mapping = {{{"all", Config::Mode::All}, {"besthits", Config::Mode::BestHits}}},
+                                      .arg    = "--search_mode",
+                                      .desc   = "do not search for reversed complements",
+                                      .value  = SearchMode::All,
+                                      .mapping = {{{"all", SearchMode::All}, {"besthits", SearchMode::BestHits}}},
+};
+auto cliMaxHits   = clice::Argument{ .parent = &cli,
+                                     .arg    = "--max_hits",
+                                     .desc   = "maximum number of hits per query",
+                                     .value  = 0,
 };
 
-
-
-auto loadConfig() {
-    auto config          = Config{};
-    config.queryPath     = *cliQuery;
-    config.indexPath     = *cliIndex;
-    config.generator     = *cliGenerator;
-    config.generator_dyn = cliDynGenerator;
-    config.k             = *cliNumErrors;
-    config.reverse       = cliNoReverse;
-    config.mode          = *cliSearchMode;
-    return config;
-
-}
 void app() {
     constexpr size_t Sigma = 5;
 
-    auto config = loadConfig();
-    auto const [queries, queryInfos] = loadQueries<Sigma>(config.queryPath, config.reverse);
+    auto const [queries, queryInfos] = loadQueries<Sigma>(*cliQuery, !cliNoReverse);
     if (queries.empty()) {
         fmt::print("query file was empty - abort\n");
         return;
@@ -89,7 +80,7 @@ void app() {
     auto name = Table::extension();
     fmt::print("start loading {} ...", name);
     fflush(stdout);
-    auto index = loadDenseIndex<CSA, Table>(config.indexPath, /*samplingRate*/16, /*threadNbr*/1);
+    auto index = loadDenseIndex<CSA, Table>(*cliIndex, /*samplingRate*/16, /*threadNbr*/1);
     fmt::print("done\n");
 
     auto memory = [&] () -> size_t {
@@ -99,18 +90,19 @@ void app() {
             return 0ull;
         }
     }();
-    auto k = config.k;
+    auto k = *cliNumErrors;
+    auto generator = *cliGenerator;
     auto search_scheme = [&]() {
-        auto iter = search_schemes::generator::all.find(config.generator);
+        auto iter = search_schemes::generator::all.find(generator);
         if (iter == search_schemes::generator::all.end()) {
-            throw std::runtime_error("unknown search scheme generetaror \"" + config.generator + "\"");
+            throw std::runtime_error("unknown search scheme generetaror \"" + generator + "\"");
         }
         auto len = queries[0].size();
         auto oss = iter->second(0, k, 0, 0); //!TODO last two parameters of second are not being used
         auto ess = search_schemes::expand(oss, len);
         auto dss = search_schemes::expandDynamic(oss, len, 4, 3'000'000'000); //!TODO use correct Sigma and text size
-        fmt::print("ss diff: {} to {}, using dyn: {}\n", search_schemes::expectedNodeCount(ess, 4, 3'000'000'000), search_schemes::expectedNodeCount(dss, 4, 3'000'000'000), config.generator_dyn);
-        if (!config.generator_dyn) {
+        fmt::print("ss diff: {} to {}, using dyn: {}\n", search_schemes::expectedNodeCount(ess, 4, 3'000'000'000), search_schemes::expectedNodeCount(dss, 4, 3'000'000'000), cliDynGenerator);
+        if (!cliDynGenerator) {
             return ess;
         } else {
             return dss;
@@ -120,15 +112,15 @@ void app() {
         auto r = std::vector<decltype(search_scheme)>{};
         for (size_t j{0}; j<=k; ++j) {
             r.emplace_back([&]() {
-                auto iter = search_schemes::generator::all.find(config.generator);
+                auto iter = search_schemes::generator::all.find(generator);
                 if (iter == search_schemes::generator::all.end()) {
-                    throw std::runtime_error("unknown search scheme generetaror \"" + config.generator + "\"");
+                    throw std::runtime_error("unknown search scheme generetaror \"" + generator + "\"");
                 }
                 auto len = queries[0].size();
                 auto oss = iter->second(j, j, 0, 0); //!TODO last two parameters of second are not being used
                 auto ess = search_schemes::expand(oss, len);
                 auto dss = search_schemes::expandDynamic(oss, len, 4, 3'000'000'000); //!TODO use correct Sigma and text size
-                if (!config.generator_dyn) {
+                if (!cliDynGenerator) {
                     return ess;
                 } else {
                     return dss;
@@ -156,12 +148,12 @@ void app() {
         resultCursorsEditTranscript.emplace_back(std::move(s));
     };
 
-    if (config.mode == Config::Mode::All) {
-        if (config.maxHitsPerQuery == 0) search_ng21::search(index, queries, search_scheme, res_cb);
-        else                             search_ng21::search_n(index, queries, search_scheme, config.maxHitsPerQuery, res_cb);
-    } else if (config.mode == Config::Mode::BestHits) {
-        if (config.maxHitsPerQuery == 0) search_ng21::search_best(index, queries, search_schemes, res_cb);
-        else                             search_ng21::search_best_n(index, queries, search_schemes, config.maxHitsPerQuery, res_cb);
+    if (*cliSearchMode == SearchMode::All) {
+        if (*cliMaxHits == 0) search_ng21::search(index, queries, search_scheme, res_cb);
+        else                  search_ng21::search_n(index, queries, search_scheme, *cliMaxHits, res_cb);
+    } else if (*cliSearchMode == SearchMode::BestHits) {
+        if (*cliMaxHits == 0) search_ng21::search_best(index, queries, search_schemes, res_cb);
+        else                  search_ng21::search_best_n(index, queries, search_schemes, *cliMaxHits, res_cb);
     }
 
     auto time_search = sw.reset();
@@ -179,7 +171,7 @@ void app() {
         list.erase(std::unique(begin(list), end(list)), list.end());
         return list;
     }(results);
-    std::unordered_set<size_t> readIds;
+    auto readIds = std::unordered_set<size_t>{};
     for (auto const& [queryId, cursor, e] : resultCursors) {
         if (queryId > queries.size()/2) {
             readIds.insert(queryId - queries.size() / 2);
@@ -190,14 +182,13 @@ void app() {
 
     fmt::print("{:15} {:3}: {:>10.3}s ({:>10.3}s+{:>10.3}s) {:>10.3}q/s - results: {:>10}/{:>10}/{:>10}/{:>10} - mem: {:>13}\n", name, k, time_search + time_locate, time_search, time_locate, queries.size() / (time_search+time_locate), resultCt, results.size(), uniqueResults.size(), readIds.size(), memory);
     {
-        if (!config.saveOutput.empty()) {
-            auto ofs = fopen(config.saveOutput.string().c_str(), "w");
-            for (auto const& [queryId, seqId, pos, e] : results) {
-//                            auto const& qi = queryInfos[queryId];
-                fmt::print(ofs, "{} {} {}\n", queryId, seqId, pos);
-            }
-            fclose(ofs);
-        }
+//        if (!config.saveOutput.empty()) {
+//            auto ofs = fopen(config.saveOutput.string().c_str(), "w");
+//            for (auto const& [queryId, seqId, pos, e] : results) {
+//                fmt::print(ofs, "{} {} {}\n", queryId, seqId, pos);
+//            }
+//            fclose(ofs);
+//        }
     }
 }
 }
