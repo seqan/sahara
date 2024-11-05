437987eeebf954a34ed9e843806181ba1c1bf985
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-11-06 00:30:17 +0100

    fix: remove lib folder

--- lib/ivio/docs/snippets/read_vcf.cpp
+++
@@ -1,9 +0,0 @@
-#include <ivio/ivio.h>
-#include <iostream>
-
-int main(int argc, char** argv) {
-    auto file = std::filesystem::path{argv[1]};
-    for (auto view : ivio::vcf::reader{{file}}) {
-        std::cout << view.ref << "\n";
-    }
-}
--- lib/ivsigma/docs/snippets/simple_example.cpp
+++
@@ -1,9 +0,0 @@
-#include <ivsigma/ivsigma.h>
-#include <iostream>
-
-int main() {
-    auto ranks = ivs::convert_char_to_rank<ivs::dna4>("acgt");
-    for (uint8_t c : ranks) {
-        std::cout << (int)c << "\n";
-    }
-}
